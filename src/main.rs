5a6bd0cfbe3d84d8e6ffeaa695c510510a8ce02d
Taylor Bergquist <knexer@gmail.com>
2024-12-31 14:16:47 -0800

    Have player assign mice on startup.

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,28 @@ const MOUSE_RADIUS: f32 = 0.4;
 // Set up project and import library for multiple mice. (done)
 // Control one entity with each discovered mouse, no speed limits. (done)
 // Clamp the entities to the screen. (done)
-// Figure out which mouse is left and which is right. Maybe ask player to move the mice to the sides of the screen?
+// Figure out which mouse is left and which is right. (done, needs polish)
 // Make the player entity move with the left mouse, and the reticle entity move with the right mouse.
 // Make the player entity move with speed/acceleration limits.
+
+// Next steps:
+// Just finished assigning left/right/none to the mice.
+// Needs some wrapping up / polish:
+// - move intro state stuff to a new plugin
+// - spawn some boxes for the mouse assignment targets
+// - despawn each box when the corresponding cursor is assigned
+// - show some intro text / instructions (in each box, and in the middle of the screen)
+// - change states when we have both mice assigned
+// - make the text and leftover cursors disappear when we change states
+// - spawn the cursors with some vertical spread instead of all on top of each other
+
+// Maybe, while we're at it, move some of the basic window/camera/cursor stuff to a new plugin.
+// It would be nice to reserve main.rs for doing new things.
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
+    .insert_state(AppState::Intro)
     .add_plugins(WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Backquote)))
     .add_systems(
       Update,
@@ -45,18 +60,122 @@ fn main() {
     )
     .add_systems(Update, close.run_if(input_just_pressed(KeyCode::Escape)))
     .add_plugins(MischiefPlugin)
-    .add_systems(Startup, spawn_cursors)
+    .add_systems(OnEnter(AppState::Intro), spawn_cursors)
     .add_systems(
       Update,
       apply_mouse_events
         .after(mischief::poll_events)
         .run_if(input_toggle_active(true, KeyCode::Backquote)),
     )
+    .add_systems(
+      Update,
+      (assign_cursor_hands, color_cursors)
+        .chain()
+        .after(apply_mouse_events)
+        .run_if(in_state(AppState::Intro)),
+    )
     .run();
 }
 
-#[derive(Component)]
-struct MouseControlled(pub Option<u32>);
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+enum AppState {
+  Intro,
+  Playing,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Hand {
+  Left,
+  Right,
+}
+
+#[derive(Component, Debug, Clone, PartialEq, Eq, Hash)]
+struct MouseControlled {
+  pub id: u32,
+  pub hand: Option<Hand>,
+}
+
+fn color_cursors(
+  mouse_controlled: Query<(&Transform, &MouseControlled, &MeshMaterial2d<ColorMaterial>)>,
+  mut materials: ResMut<Assets<ColorMaterial>>,
+  window_query: Query<&Window>,
+  camera_query: Query<(&GlobalTransform, &OrthographicProjection), With<Camera>>,
+) {
+  let window = window_query.single();
+  let window_to_world = {
+    let (camera_transform, projection) = camera_query.single();
+    |position: Vec2| window_to_world(window, camera_transform, projection, position)
+  };
+
+  let needs_left = !mouse_controlled
+    .iter()
+    .any(|(_, mc, _)| mc.hand == Some(Hand::Left));
+  let needs_right = !mouse_controlled
+    .iter()
+    .any(|(_, mc, _)| mc.hand == Some(Hand::Right));
+
+  let left_color = Color::hsl(30., 0.95, 0.7);
+  let left_base_none_color = Color::hsl(30., 0.0, 0.7);
+  let right_color = Color::hsl(150., 0.95, 0.7);
+  let right_base_none_color = Color::hsl(150., 0.0, 0.7);
+  for (transform, mouse_controlled, material) in mouse_controlled.iter() {
+    let new_color = match mouse_controlled.hand {
+      Some(Hand::Left) => left_color,
+      Some(Hand::Right) => right_color,
+      None => {
+        let left_edge = window_to_world(Vec2::new(window.width() / 4.0, 0.0)).x;
+        let right_edge = window_to_world(Vec2::new(window.width() * 3.0 / 4.0, 0.0)).x;
+        let center = (left_edge + right_edge) / 2.0;
+        let proportion_to_right = (transform.translation.x - center) / (right_edge - center);
+        let proportion_to_left = -proportion_to_right;
+
+        if proportion_to_left > 0. && needs_left {
+          left_base_none_color.mix(&left_color, proportion_to_left)
+        } else if proportion_to_right > 0. && needs_right {
+          right_base_none_color.mix(&right_color, proportion_to_right)
+        } else {
+          left_base_none_color
+        }
+      }
+    };
+    materials.insert(material, new_color.into());
+  }
+}
+
+fn assign_cursor_hands(
+  mut mouse_controlled: Query<(&Transform, &mut MouseControlled)>,
+  window_query: Query<&Window>,
+  camera_query: Query<(&GlobalTransform, &OrthographicProjection), With<Camera>>,
+) {
+  let window = window_query.single();
+  let window_to_world = {
+    let (camera_transform, projection) = camera_query.single();
+    |position: Vec2| window_to_world(window, camera_transform, projection, position)
+  };
+
+  let needs_left = !mouse_controlled
+    .iter()
+    .any(|(_, mc)| mc.hand == Some(Hand::Left));
+  let needs_right = !mouse_controlled
+    .iter()
+    .any(|(_, mc)| mc.hand == Some(Hand::Right));
+
+  for (transform, mut mouse_controlled) in mouse_controlled.iter_mut() {
+    if mouse_controlled.hand.is_some() {
+      continue;
+    }
+
+    if needs_left
+      && transform.translation.x < window_to_world(Vec2::new(window.width() / 4.0, 0.0)).x
+    {
+      mouse_controlled.hand = Some(Hand::Left);
+    } else if needs_right
+      && transform.translation.x > window_to_world(Vec2::new(window.width() * 3.0 / 4.0, 0.0)).x
+    {
+      mouse_controlled.hand = Some(Hand::Right);
+    }
+  }
+}
 
 fn spawn_cursors(
   mut commands: Commands,
@@ -67,13 +186,12 @@ fn spawn_cursors(
   for (i, mouse) in mischief_session.session.devices.iter().enumerate() {
     commands.spawn((
       Transform::default(),
-      MouseControlled(Some(mouse.id)),
+      MouseControlled {
+        id: mouse.id,
+        hand: None,
+      },
       Mesh2d::from(meshes.add(RegularPolygon::new(MOUSE_RADIUS, 3u32 + i as u32))),
-      MeshMaterial2d(materials.add(Color::hsl(
-        360. * i as f32 / mischief_session.session.devices.len() as f32,
-        0.95,
-        0.7,
-      ))),
+      MeshMaterial2d(materials.add(Color::WHITE)),
     ));
   }
 }
@@ -92,7 +210,7 @@ fn apply_mouse_events(
 
   for event in mouse_events.read() {
     for (mut transform, mouse_controlled) in mouse_controlled.iter_mut() {
-      if mouse_controlled.0 != Some(event.device) {
+      if mouse_controlled.id != event.device {
         continue;
       }
       match event.event_data {
